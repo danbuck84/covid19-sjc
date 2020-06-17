20e4a66e1139cfeaf5ab93e08099ad542022053b
Dan Buck <35202075+danbuck84@users.noreply.github.com>
2020-06-17 10:52:22 -0300

    new data sheet, new other assets and some new implementation on some scripts and main.html

--- test/scripts/positiveCasesDailyGraph.js
+++ test/scripts/positiveCasesDailyGraph.js
@@ -73,3 +73,79 @@ async function getData() {
     
     return { xs, yPositiveDaily };
 }
+
+positiveCasesDailyTotal();
+
+async function positiveCasesDailyTotal() {
+    const data = await getDatas();
+    const ctx = document.getElementById('positive-daily-total').getContext('2d');
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        hover: {
+            mode: 'average',
+            intersect: false
+        },
+        scales: {
+            yAxes: [{
+                display: true,
+                gridLines: {
+                    display: true,
+                    color: "rgba(255, 255, 255, 0.05)"
+                },
+                stacked: false
+            }],
+            xAxes: [{
+                label: 'Casos',
+                display: true,
+                gridLines: {
+                    display: true,
+                    color: ['rgba(255, 255, 255, 0.5)']
+                },
+            }]
+        }
+    };
+    const graph = {
+        labels: data.xs,
+        datasets: [
+            {
+                label: 'Casos diários',
+                data: data.yPositiveDaily,
+                backgroundColor: ['rgba(0, 65, 139, 0.5)'],
+                borderColor: ['rgba(0, 65, 139, 1)'],
+                borderWidth: 2,
+                lineTension: 0.5,
+                fill: true
+            }
+        ]
+    };
+    const myChart = new Chart(ctx, {
+        options: options,
+        type: 'bar',
+        data: graph,
+    });
+}
+    
+async function getDatas() {
+    const xs = [];
+    const yPositiveDaily = [];
+    
+    const response = await fetch('../assets/data.csv');
+    const data = await response.text();
+    
+    const table = data.split('\n').slice(1);
+    table.forEach(row => {
+        const columns = row.split(',');
+        const days = columns[0];
+        const positiveDaily = columns[4];
+
+        xs.push(days);
+        yPositiveDaily.push(positiveDaily);
+    });
+    
+    return { xs, yPositiveDaily };
+}
